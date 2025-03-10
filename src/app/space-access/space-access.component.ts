12db7c23e71157803c2a4d793fd93574369a4d4d
tncsito <eduardosantoyo5322100257@correcaminos.uts.edu.mx>
2025-03-10 09:05:52 -0700

    Componentes

---
+++ src/app/space-access/space-access.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-space-access',
+  imports: [],
+  templateUrl: './space-access.component.html',
+  styleUrl: './space-access.component.css'
+})
+export class SpaceAccessComponent {
+
+}
